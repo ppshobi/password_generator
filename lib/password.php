8691f77b28121bfb66199251ee7cb7a6a26a5ba3
Shobi P P <ppshobi@gmail.com>
2016-04-14 18:19:38 +0530

    Added pickrandom function to pick a random number

--- lib/password.php
+++ lib/password.php
@@ -4,14 +4,14 @@
 */
 class Password {
 	
-	$lower_letters="abcdefghijklmnopqrstuvwxyz";
-	$upper_letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	$numbers="0123456789";
-	$symbols="!@#$%^&*()_-+=~`.,";
+	// $lower_letters="abcdefghijklmnopqrstuvwxyz";
+	// $upper_letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	// $numbers="0123456789";
+	// $symbols="!@#$%^&*()_-+=~`.,";
 
-	//charset
-	$charset="";
-
+	// charset
+	// $charset='';
+	// $generated_password='';
 
 	function __construct()
 	{
@@ -19,21 +19,33 @@ class Password {
 	}
 
 	function define_charset($options){
-		if ($options['lower']) {
-			this->$charset .= $lower_letters;
-		}if ($options['upper']) {
-			this->$charset .= $upper_letters;
-		}if ($options['numbers']) {
-			this->$charset .= $numbers;
-		}if ($options['symbols']) {
-			this->$charset .= $symbols;
-		}
+		// if ($options['lower']) {
+			this->$charset .= this->$lower_letters;
+		// }if ($options['upper']) {
+			this->$charset .= this->$upper_letters;
+		// }if ($options['numbers']) {
+			this->$charset .= this->$numbers;
+		// }if ($options['symbols']) {
+			this->$charset .= this->$symbols;
+		// }
+	// }
+
+	function pickrandomno($start=0,$end){
+		return mt_rand($start,$end);
 	}
 
 	function generate($length,$options){
 		define_charset($options);
+		
+		$charset_length=strlen(this->charset);
 
-		return "Foo";
+		
+		for(i=0;i<$length;i++){
+			$position=pickrandomno(0,$charset_length-1);
+			this->$generated_password.=this->charset[$position];
+		}
+
+		return $generated_password;
 	}
 }
 
